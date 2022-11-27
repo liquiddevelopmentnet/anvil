62c7fdc89ceb6ec58b628dd1eb63f59abc616b9b
liquiddevelopmentnet <info@minemix.de>
2022-11-27 18:04:17 +0100

    feat: projects config

--- anvil-server/src/config/main.rs
+++ anvil-server/src/config/main.rs
@@ -1,15 +1,49 @@
-use std::io::{Read, Write};
-use std::path::Path;
-use std::process::exit;
-use serde::Serialize;
+use serde::{Serialize};
 use serde::Deserialize;
 use crate::*;
+use crate::config::{save_config, YamlConfig};
 
-static mut CONFIG: Option<Configuration> = None;
-const DEFAULT_CONFIG_NAME: &str = "anvil-config.yml";
+static mut CONFIG: Option<MainConfiguration> = None;
+
+impl YamlConfig for MainConfiguration {
+    const DEFAULT_CONFIG_NAME: &'static str = "anvil-config.yml";
+
+    fn new() -> Self {
+        MainConfiguration {
+            bind_config: BindConfig {
+                address: "localhost".to_string(),
+                port: 56971,
+            },
+            database_config: DatabaseConfig {
+                address: "localhost".to_string(),
+                port: 5432,
+                username: "root".to_string(),
+                password: "password".to_string(),
+                database: "anvil".to_string(),
+            },
+            plugins_enabled: false,
+            notification_config: NotificationConfig {
+                enabled: true,
+                webhooks: vec![
+                    WebhookConfig {
+                        name: "anvil Notifications".to_string(),
+                        url: "https://discord.com/api/webhooks/1234567890/abcdefghijklmnopqrstuvwxyz".to_string(),
+                        avatar_url: Some("https://example.com/yourimage.png".to_string()),
+                        method: "POST".to_string(),
+                        standard: WebhookStandard::Discord,
+                    },
+                ],
+            },
+        }
+    }
+
+    fn save(&self) -> Result<(), io::Error> {
+        save_config(self, Self::DEFAULT_CONFIG_NAME)
+    }
+}
 
 #[derive(Serialize, Deserialize, Clone)]
-pub struct Configuration {
+pub struct MainConfiguration {
     pub bind_config: BindConfig,
     pub database_config: DatabaseConfig,
     pub plugins_enabled: bool,
@@ -53,63 +87,19 @@ pub enum WebhookStandard {
     Generic,
 }
 
-impl Configuration {
-    fn new() -> Self {
-        Self {
-            bind_config: BindConfig {
-                address: "localhost".to_string(),
-                port: 56971,
-            },
-            database_config: DatabaseConfig {
-                address: "localhost".to_string(),
-                port: 5432,
-                username: "root".to_string(),
-                password: "password".to_string(),
-                database: "anvil".to_string(),
-            },
-            plugins_enabled: false,
-            notification_config: NotificationConfig {
-                enabled: true,
-                webhooks: vec![
-                    WebhookConfig {
-                        name: "anvil Notifications".to_string(),
-                        url: "https://discord.com/api/webhooks/1234567890/abcdefghijklmnopqrstuvwxyz".to_string(),
-                        avatar_url: Some("https://example.com/yourimage.png".to_string()),
-                        method: "POST".to_string(),
-                        standard: WebhookStandard::Discord,
-                    },
-                ],
-            },
+pub fn load() -> bool {
+    unsafe {
+        let load = config::load_config(MainConfiguration::DEFAULT_CONFIG_NAME);
+        if load.is_some() {
+            CONFIG = load;
+            false
+        } else {
+            true
         }
     }
-
-    pub fn save(&self) -> Result<(), std::io::Error> {
-        let yaml = serde_yaml::to_string(&self).unwrap();
-
-        let mut file = std::fs::File::create(DEFAULT_CONFIG_NAME).unwrap();
-        file.write_all(yaml.as_bytes()).unwrap();
-
-        Ok(())
-    }
 }
 
-pub fn load() {
-    if Path::new(DEFAULT_CONFIG_NAME).exists() {
-        let mut file = std::fs::File::open(DEFAULT_CONFIG_NAME).unwrap();
-        let mut contents = String::new();
-        file.read_to_string(&mut contents).unwrap();
-
-        let config: Configuration = serde_yaml::from_str(&contents).unwrap();
-        unsafe { CONFIG = Option::from(config); }
-    } else {
-        let new = Configuration::new();
-        new.save().unwrap();
-        info!("A {} file has been created in the current directory. Please configure it to your liking and restart the server.", DEFAULT_CONFIG_NAME);
-        exit(0);
-    }
-}
-
-pub fn get() -> Configuration {
+pub fn get() -> MainConfiguration {
     unsafe {
         CONFIG.as_ref().unwrap().clone()
     }
